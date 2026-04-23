9d4b1fe98754e2d71b1039db8d98442ea4948bfa
juandentt <panchesmacias04@gmail.com>
2026-04-23 09:59:42 +0200

    feat: API call for Pokémon Id

--- app/src/main/java/com/example/pokedex/data/network/PokeApiService.kt
+++ app/src/main/java/com/example/pokedex/data/network/PokeApiService.kt
@@ -1,10 +1,14 @@
 package com.example.pokedex.data.network
 
+import com.example.pokedex.data.model.PokemonDetail
 import com.example.pokedex.data.model.PokemonList
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokeApiService {
     @GET("pokemon?limit=20&offset=0")
     suspend fun getPokemonList(): PokemonList
 
+    @GET("pokemon/{id}")
+    suspend fun getPokemonDetail(@Path("id") id: Int): PokemonDetail
 }
