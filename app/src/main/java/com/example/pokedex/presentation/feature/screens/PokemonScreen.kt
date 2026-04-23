e663893d105898ff0f32f7f1688dd59fb83d135a
juandentt <panchesmacias04@gmail.com>
2026-04-23 10:03:54 +0200

    feat: PokemonItem parameters

--- app/src/main/java/com/example/pokedex/presentation/feature/screens/PokemonScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/feature/screens/PokemonScreen.kt
@@ -7,21 +7,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import com.example.pokedex.presentation.state.PokemonUiState
 import com.example.pokedex.presentation.components.PokemonItem
 import com.example.pokedex.presentation.feature.viewmodel.PokemonViewModel
 
 
 @Composable
-fun PokemonScreen(viewModel: PokemonViewModel = viewModel()) {
+fun PokemonScreen(
+    viewModel: PokemonViewModel = viewModel(),
+    navController: NavHostController
+) {
 
     when (val state = viewModel.uiState) {
 
-        is PokemonUiState.Loading -> { CircularProgressIndicator() }
+        is PokemonUiState.Loading -> {
+            CircularProgressIndicator()
+        }
+
         is PokemonUiState.Success -> {
             LazyColumn {
                 items(state.pokemons) { pokemon ->
-                    PokemonItem(pokemon)
+                    PokemonItem(pokemon, navController, viewModel)
                 }
             }
         }
