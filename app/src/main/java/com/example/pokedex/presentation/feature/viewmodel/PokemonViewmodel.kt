c8392e51c8ff1f2ca73a3ddd9ec46331aa25d368
juandentt <panchesmacias04@gmail.com>
2026-04-23 10:01:16 +0200

    feat: Selected Pokémon and Pokémon Detail

--- app/src/main/java/com/example/pokedex/presentation/feature/viewmodel/PokemonViewmodel.kt
+++ app/src/main/java/com/example/pokedex/presentation/feature/viewmodel/PokemonViewmodel.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pokedex.data.model.PokemonDetail
+import com.example.pokedex.data.model.PokemonResult
 import com.example.pokedex.data.network.RetrofitClient
 import com.example.pokedex.presentation.state.PokemonUiState
 import kotlinx.coroutines.launch
@@ -12,6 +14,10 @@ import kotlinx.coroutines.launch
 class PokemonViewModel : ViewModel() {
     private var _uiState by mutableStateOf<PokemonUiState>(PokemonUiState.Loading)
     val uiState: PokemonUiState get() = _uiState
+
+    var selectedPokemon: PokemonResult? = null
+    private var _pokemonDetail by mutableStateOf<PokemonDetail?>(null)
+    val pokemonDetail: PokemonDetail? get() = _pokemonDetail
 
     init {
         loadPokemons()
@@ -24,6 +30,17 @@ class PokemonViewModel : ViewModel() {
                 _uiState = PokemonUiState.Success(response.results)
             } catch (_: Exception) {
                 _uiState = PokemonUiState.Error("Error al cargar")
+            }
+        }
+    }
+
+    fun loadPokemonDetail(id: Int) {
+        viewModelScope.launch {
+            try {
+                _pokemonDetail =
+                    RetrofitClient.instance.getPokemonDetail(id)
+            } catch (_: Exception) {
+                _pokemonDetail = null
             }
         }
     }
