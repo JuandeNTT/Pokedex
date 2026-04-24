cfacad22190b1b5b2577a683b071695e82d3bb49
juandentt <panchesmacias04@gmail.com>
2026-04-24 10:04:34 +0200

    feat: Button UI upgrade

--- app/src/main/java/com/example/pokedex/presentation/feature/screens/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokedex/presentation/feature/screens/PokemonDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.pokedex.presentation.feature.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,16 +8,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -97,14 +104,24 @@ fun PokemonDetailScreen(
                 }
             }
         }
-    Button(
-        onClick = {
-            navController.popBackStack()
-        },
-        modifier = Modifier
-            .padding(16.dp)
-            .align(Alignment.CenterHorizontally)
-    ) {
+        OutlinedButton(
+            onClick = {
+                navController.popBackStack()
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White
+            ),
+            border = BorderStroke(1.dp, Color.Red)
+        ) {
+        Icon(
+            Icons.Default.ArrowBack,
+            contentDescription = "Volver al listado"
+        )
+        Spacer(Modifier.width(10.dp))
         Text("Volver al listado")
     }
     }
